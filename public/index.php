8c929bfbadd2d5f3f537b8763aaea14231e74189
AbdullahKaram-Dev <abdallakaramdev@gmail.com>
2021-12-06 04:22:36 +0200

    try strategy pattern (validation php)

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 $rootPath = dirname(__DIR__);
 
 define("TRANSLATION_PATH",$rootPath.'/translation/');
-
 define("USER_LOCALE","en");
 
 use app\controllers\ContactController;
@@ -19,18 +18,14 @@ $app = new Application($rootPath);
 
 
 $app->router->get('/',[HomeController::class,'index']);
-
 $app->router->get('/contact',[ContactController::class,'create']);
 $app->router->post('/contact/create',[ContactController::class,'store']);
-
 $app->router->get('/users',[UserController::class,'index']);
 $app->router->get('/user/create',[UserController::class,'create']);
-
 $app->router->get('/login',[AuthController::class,'index']);
 $app->router->post('/login',[AuthController::class,'index']);
 $app->router->get('/register',[AuthController::class,'register']);
 $app->router->post('/register',[AuthController::class,'register']);
-
 $app->router->post('/translation',[TrnaslationController::class,'setTranslation']);
 
 $app->run();
